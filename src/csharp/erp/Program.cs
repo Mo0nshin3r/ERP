cb1bb5dc67e37dd1851be3bd4d53201d43a11f46
Anthony Victor <pe27igm@eduvaud.ch>
2024-01-10 14:07:27 +0100

    connection to db via vs

--- src/csharp/erp/Program.cs
+++ src/csharp/erp/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MySqlConnector;
+using System.Data;
+
+string m_strMySQLConnectionString;
+m_strMySQLConnectionString = "server=localhost;userid=root;database=ERP;port=6033;password=root";
+
+GetValueFromDBUsing("select * from dual");
+
+string GetValueFromDBUsing(string strQuery)
+{
+    string strData = "";
+
+    
+        if (string.IsNullOrEmpty(strQuery) == true)
+            return string.Empty;
+
+        using (var mysqlconnection = new MySqlConnection(m_strMySQLConnectionString))
+        {
+            mysqlconnection.Open();
+            using (MySqlCommand cmd = mysqlconnection.CreateCommand())
+            {
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandTimeout = 300;
+                cmd.CommandText = strQuery;
+
+                object objValue = cmd.ExecuteScalar();
+                if (objValue == null)
+                {
+                    cmd.Dispose();
+                    return string.Empty;
+                }
+                else
+                {
+                    strData = (string)cmd.ExecuteScalar();
+                    cmd.Dispose();
+                }
+
+                mysqlconnection.Close();
+
+                if (strData == null)
+                    return string.Empty;
+                else
+                    return strData;
+            }
+        }
+    
+}
+
+
+
+/*void LogException(MySqlException ex)
+{
+    throw new NotImplementedException();
+}
+*/
